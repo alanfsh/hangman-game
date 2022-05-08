a9980c5f5e10d2ab9540989b120781afed14651f
zero <zero_ikniz@hotmail.com>
2022-05-07 23:31:25 -0500

    Agregada impresion de letras utilizadas

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def select_word():
     return random_word.upper()
 
 
-def draw_man(lifes):
+def draw_man(lifes, repeated_letter):
     hang_image = ["  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
@@ -24,22 +24,29 @@ def draw_man(lifes):
     print("Vidas: " + str(lifes))
     print(hang_image[lifes])
     print("")
+    if repeated_letter:
+        print("Ya utilizaste esa letra!!!")            
+
 
 
 def game(word):
     lifes = 6
+
     # Characters accepted
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
     special_char = ['Á', 'É', 'Í', 'Ó', 'Ú','Ü',]
     accepted_char = alphabet + special_char
+    used_letters = set()   # Used letters 
+    repeated_letter = False  # If the user inputs a letter used
 
     found_letters = ["_"] * len(word)  # All letters found
     user_word= "".join(found_letters)  # String that contains the letters found
     os.system("clear")   
 
     while user_word != word and lifes > 0:
-        draw_man(lifes)
+        draw_man(lifes, repeated_letter)
         print(user_word.replace("", " ") + "\n")  # Prints the incomplete word with "_"
+        print("Letras utilizadas: " + ", ".join(used_letters) + "\n")  # Prints the used letters
         try: 
             letter = input("Ingresa una letra: ").upper()
             if len(letter) > 1:  # User only can type 1 character
@@ -48,16 +55,23 @@ def game(word):
             if letter not in accepted_char:
                 raise TypeError("Solo puedes ingresar letras")
 
-            if letter in word:
-                for index, character in enumerate(word):
-                    if letter == character:
-                        found_letters[index] = letter  # Replace the "_" with the user letter
+            if letter in used_letters:
+                repeated_letter = True
 
-            if letter not in word:
-                lifes -= 1
+            if letter not in used_letters:
+                if letter in word:
+                    for index, character in enumerate(word):
+                        if letter == character:
+                            found_letters[index] = letter  # Replace the "_" with the user letter
+                            used_letters.add(letter)  # Add letter to the used letters
+                if letter not in word:
+                    used_letters.add(letter)
+                    lifes -= 1
+                repeated_letter = False
                 
             user_word= "".join(found_letters)
             os.system("clear")
+    
         except TypeError as te:
             os.system("clear")
             print(te)
@@ -68,7 +82,7 @@ def game(word):
             print("")
     
     os.system("clear")
-    draw_man(lifes)
+    draw_man(lifes, repeated_letter)
     print(user_word.replace("", " ") + "\n")
     if lifes > 0:
         print("¡Ganaste! La palabra es " + word + "\n")
